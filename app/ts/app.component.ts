32e798d63bca4abf9f9826a0cbfe0d999dd205a0
Tsinat Zeree <cheru87@gmail.com>
2016-09-17 15:38:46 -0700

    displaying list of videos

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,11 +1,22 @@
 import {Component} from 'angular2/core';
 import { config } from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 {{ mainHeading }}Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
 export class AppComponent {
     mainHeading = config.MAIN_HEADING;
+    videos: Array<Video>;
+
+    constructor(){
+        this.videos = [
+            new Video(1, 'installing Django', 'qgGIqRFvFFK', 'how to install Django'),
+            new Video(2, 'Nested Component', 'f8qBeaGe2S4', 'how to create angular2 components')
+        ]
+    }
 }
--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'playlist',
-    templateUrl: 'app/ts/playlist.component.html'
+    templateUrl: 'app/ts/playlist.component.html',
+    inputs: ['videos']
 })
 
 export class PlaylistComponent{}
